1f7e4b3c7d47c63d624d956e23b4f8bdf6d2c062
Samet Ateş <74558179+sametates4@users.noreply.github.com>
2023-04-18 00:41:20 +0300

    ImageNetwork widget added

--- lib/src/widget/image_network.dart
+++ lib/src/widget/image_network.dart
@@ -4,17 +4,18 @@ import 'package:modrabl/modrabl.dart';
 class ImageNetwork extends StatelessWidget {
   const ImageNetwork({Key? key, required this.imageUrl}) : super(key: key);
   final String imageUrl;
+
   @override
   Widget build(BuildContext context) {
     return Image.network(
-        imageUrl,
+      imageUrl,
       loadingBuilder: (context, child, loadingProgress) {
         if (loadingProgress == null) return child;
         return Center(
           child: CircularProgressIndicator(
             value: loadingProgress.expectedTotalBytes != null
                 ? loadingProgress.cumulativeBytesLoaded /
-                loadingProgress.expectedTotalBytes!
+                    loadingProgress.expectedTotalBytes!
                 : null,
           ),
         );
